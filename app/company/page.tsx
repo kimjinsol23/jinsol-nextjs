2401250662ad4b44040743f635a096a084096c39
김진솔 <kimjinsol@JESSI-MacBook-Air.local>
2026-02-25 10:09:09 +0900

    first commit

---
+++ app/company/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Company() {
+  return (
+    <div className={styles.abc}>
+      <h1>안녕. 여긴 Company페이지야</h1>
+      <p className={styles.test}>이건 설명글</p>
+      <div>hahahahaha</div>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  
+  var a = 1;
+  var b = 5;
+  var totla = a + b;
+  console.log(totla);
+
+  var items = [1, 2, 3, 4, 5, 6];
+
+  return (
+    <div>
+      메인페이지<br/>
+      <a href="http://localhost:3000/company">Company 이동</a><br/>
+      <Link href="/company">회사페이지{totla}</Link>
+      <div>
+        <ul>
+          {items.map((item) => (
+          <li key={item}>리스트 {item}</li>
+        ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
